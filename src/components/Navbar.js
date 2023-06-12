dc6477e26f98e3b6fc7af4e0c375000c7522ff4b
Mohamed Jalloul <mjalloul@e2r5p14.1337.ma>
2023-06-12 11:05:45 +0100

    d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,7 +81,7 @@ const Navbar = () => {
       >
         <HorizontalStack alignItems="end"  sx={{ textDecoration: 'none' }}  component={Link}  to={"/"}>
           <Box >
-          <img src="./Logo.svg" alt="Image" width={20} />
+          <img src="https://real-one.vercel.app/logo.svg" alt="Image" width={20} />
           </Box>
           <Typography
             sx={{ display: mobile ? "none" : "block" }}
