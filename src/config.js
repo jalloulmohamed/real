97ba7b9652be7bd79c0565aeb0d4b62c13721d99
Mohamed Jalloul <mjalloul@e2r13p2.1337.ma>
2023-06-12 09:44:37 +0100

    d

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://reality-wwdy.onrender.com";
+let BASE_URL = "https://reality-7ea1.onrender.com";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
