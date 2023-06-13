70f82d7e87175516043c2fb125bc7b8d7cd003d6
Mohamed Jalloul <mjalloul@e2r2p15.1337.ma>
2023-06-14 00:49:10 +0100

    s

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -144,7 +144,7 @@ const PostCard = (props) => {
                         boxShadow:"rgba(0, 0, 0, 0.16) 0px 1px 4px", 
                         display:"flex" , 
                         flexDirection:"row",
-                        top:"24px", 
+                        top:"25px", 
                         right:"2px"}} >
                       <IconButton
                         disabled={loading}
