753741d4c86f85f8195437cdb64dc429c823eb1c
Mohamed Jalloul <mjalloul@e2r5p15.1337.ma>
2023-06-16 12:54:22 +0100

    d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,6 +42,9 @@ const Navbar = () => {
   const [notefication, setNotefication] = useState(false);
   const [pathnam, setPatname] = useState("");
   const location = useLocation();
+ 
+
+
 
   useEffect(() => {
     updateDimensions();
@@ -76,10 +79,7 @@ const Navbar = () => {
     setSearchIcon(!searchIcon);
   };
 
-  console.log(location)
-
   const handleNotifcation= ()=>{
-    console.log(location.pathname)
     if(location.pathname =='/messenger')
     {
       setNotefication(false);
@@ -89,6 +89,7 @@ const Navbar = () => {
       setNotefication(true)
     }
   }
+
   const handleClickNotifcation= ()=>{
        setNotefication(false);
   }
@@ -96,96 +97,99 @@ const Navbar = () => {
     socket.on("receive-message", handleNotifcation);
   }, []);
 
+
   return (
-    <Container>
+    <>
+        <Container >
+          <Stack mb={2}>
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{
+                pt: 2,
+                pb: 0,
+              }}
+              spacing={!mobile ? 2 : 0}
+            >
+              <HorizontalStack alignItems="end"  sx={{ textDecoration: 'none' }}  component={Link}  to={"/"}>
+                <Box >
+                <img src="/logo.svg" alt="Image" width={26} />
+                </Box>
+                <Typography
+                  sx={{ display: mobile ? "none" : "block" }}
+                  variant={navbarWidth ? "h5" : "h5"}
+                  color="#566376"
+                >
+                  {/* <Link to="/" color="inherit"> */}
+                    eal
+                  {/* </Link> */}
+                </Typography>
+              </HorizontalStack>
 
-    <Stack mb={2}>
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        sx={{
-          pt: 2,
-          pb: 0,
-        }}
-        spacing={!mobile ? 2 : 0}
-      >
-        <HorizontalStack alignItems="end"  sx={{ textDecoration: 'none' }}  component={Link}  to={"/"}>
-          <Box >
-          <img src="/logo.svg" alt="Image" width={26} />
-          </Box>
-          <Typography
-            sx={{ display: mobile ? "none" : "block" }}
-            variant={navbarWidth ? "h5" : "h5"}
-            color="#566376"
-          >
-            {/* <Link to="/" color="inherit"> */}
-              eal
-            {/* </Link> */}
-          </Typography>
-        </HorizontalStack>
+              {/* {!navbarWidth && (
+                <Box component="form" onSubmit={handleSubmit}>
+                  <TextField
+                    size="small"
+                    label="Search for posts..."
+                    sx={{ flexGrow: 1, maxWidth: 300 }}
+                    onChange={handleChange}
+                    value={search}
+                  />
+                </Box>
+              )} */}
 
-        {/* {!navbarWidth && (
-          <Box component="form" onSubmit={handleSubmit}>
-            <TextField
-              size="small"
-              label="Search for posts..."
-              sx={{ flexGrow: 1, maxWidth: 300 }}
-              onChange={handleChange}
-              value={search}
-            />
-          </Box>
-        )} */}
+              <HorizontalStack>
+                {/* {mobile && (
+                  <IconButton onClick={handleSearchIcon}>
+                    <IoSearchOutline size={24} color="#566376"/>
+                  </IconButton>
+                )} */}
+                
 
-        <HorizontalStack>
-          {/* {mobile && (
-            <IconButton onClick={handleSearchIcon}>
-              <IoSearchOutline size={24} color="#566376"/>
-            </IconButton>
-          )} */}
-          
+                {/* <IconButton component={Link}  to={"/"}>
+                  <AiFillHome size={20} color="#566376" />
+                </IconButton> */}
+                {user ? (
+                  <>
+                    <IconButton onClick={handleClickNotifcation} className={notefication ? 'notificationDot' : ''} component={Link} to={"/messenger"}>
+                      <AiFillMessage size={24} color="#566376" />
 
-          {/* <IconButton component={Link}  to={"/"}>
-            <AiFillHome size={20} color="#566376" />
-          </IconButton> */}
-          {user ? (
-            <>
-              <IconButton onClick={handleClickNotifcation} className={notefication ? 'notificationDot' : ''} component={Link} to={"/messenger"}>
-                <AiFillMessage size={24} color="#566376" />
+                    </IconButton>
+                    <IconButton  onClick={handleLogout}>
+                      <FiLogOut size={22} color="#566376" ></FiLogOut>
+                    </IconButton>
+                    <IconButton component={Link}  to={"/users/" + username}>
+                      <UserAvatar width={38} height={38} username={user.username} />
+                    </IconButton>
+                  </>
+                ) : (
+                  <>
+                    <Button variant="text" sx={{   minWidth: 65  , color: "#FDC04D" ,'&:hover': {backgroundColor: '#fff'}}} href="/login">
+                      Login
+                    </Button>
+                    <Button variant="text" sx={{ minWidth: 80 ,  backgroundColor : "#566376", color: "#FFF", '&:hover': {backgroundColor: '#566376'}}} href="/signup">
+                      Sign Up
+                    </Button>
+                  </>
+                )}
+              </HorizontalStack>
+            </Stack>
+            {navbarWidth && searchIcon && (
+              <Box component="form" onSubmit={handleSubmit} mt={2}>
+                <TextField
+                  size="small"
+                  label="Search for posts..."
+                  fullWidth
+                  onChange={handleChange}
+                  value={search}
+                />
+              </Box>
+            )}
+          </Stack>
+          </Container>
 
-              </IconButton>
-              <IconButton  onClick={handleLogout}>
-                <FiLogOut size={22} color="#566376" ></FiLogOut>
-              </IconButton>
-              <IconButton component={Link}  to={"/users/" + username}>
-                <UserAvatar width={38} height={38} username={user.username} />
-              </IconButton>
-            </>
-          ) : (
-            <>
-              <Button variant="text" sx={{   minWidth: 65  , color: "#FDC04D" ,'&:hover': {backgroundColor: '#fff'}}} href="/login">
-                Login
-              </Button>
-              <Button variant="text" sx={{ minWidth: 80 ,  backgroundColor : "#566376", color: "#FFF", '&:hover': {backgroundColor: '#566376'}}} href="/signup">
-                Sign Up
-              </Button>
-            </>
-          )}
-        </HorizontalStack>
-      </Stack>
-      {navbarWidth && searchIcon && (
-        <Box component="form" onSubmit={handleSubmit} mt={2}>
-          <TextField
-            size="small"
-            label="Search for posts..."
-            fullWidth
-            onChange={handleChange}
-            value={search}
-          />
-        </Box>
-      )}
-    </Stack>
-    </Container>
+    </>
   );
 };
 
