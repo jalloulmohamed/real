b7b97330ad7377f479fded036bf9f2f24413a54a
Mohamed Jalloul <mjalloul@e2r5p15.1337.ma>
2023-06-16 04:30:30 +0100

    d

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = ({ label }) => {
   return (
     <Stack alignItems="center">
-      <CircularProgress size={50} sx={{ my: 1 }} />
+      <CircularProgress size={25} sx={{ my: 1 }} />
       <Typography color="#4A92FF" sx={{ mb: 3 }}>
         {label}
       </Typography>
