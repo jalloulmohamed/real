7e4e14a4afe80744688be813ff6b7c141132faec
Mohamed Jalloul <mjalloul@e3r11p5.1337.ma>
2023-07-15 03:23:14 +0100

    d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -179,7 +179,7 @@ const Navbar = () => {
                   </>
                 ) : (
                   <>
-                    <Button variant="text" sx={{   py:"4px",  fontSize:"13px",   color: "#18181b"  ,'&:hover': {backgroundColor: '#fff'}}} href="/login">
+                    <Button variant="text" sx={{   py:"4px",  fontSize:"15px",   color: "#18181b"  ,'&:hover': {backgroundColor: '#fff'}}} href="/login">
                       Login
                     </Button>
                     <Button variant="text" sx={{py:"4px", fontSize:"13px", backgroundColor : "#18181b", color: "#FFF", '&:hover': {backgroundColor: '#18181b'}}} href="/signup">
