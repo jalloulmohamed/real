ce6e6187797168adca9c038c558657f1d9dfb1fe
Mohamed Jalloul <mjalloul@e3r10p2.1337.ma>
2023-07-16 04:33:11 +0100

    d

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -36,11 +36,11 @@ export const BottomNav = () => {
           //   setValue(newValue);
           // }}
         >
-          <BottomNavigationAction sx={{px:0}} to={"/"} icon={<GoHomeFill size={25} color='#18181b' />} />
-          <BottomNavigationAction sx={{px:0}} icon={<GoSearch size={25} color='#18181b'/>} />
-          <BottomNavigationAction  sx={{px:0}} icon={<FiFeather size={25} color='#18181b'/>} />
-          <BottomNavigationAction  sx={{px:0}} icon={<BiNotification size={25} color='#18181b'/>} />
-          <BottomNavigationAction  sx={{px:0}} component={Link}  to={"/users/" + username}  icon={<UserAvatar width={30} height={30}  />} />
+          <BottomNavigationAction sx={{px:0,minWidth:0}} to={"/"} icon={<GoHomeFill size={25} color='#18181b' />} />
+          <BottomNavigationAction sx={{px:0,minWidth:0}} icon={<GoSearch size={25} color='#18181b'/>} />
+          <BottomNavigationAction  sx={{px:0,minWidth:0}} icon={<FiFeather size={25} color='#18181b'/>} />
+          <BottomNavigationAction  sx={{px:0,minWidth:0}} icon={<BiNotification size={25} color='#18181b'/>} />
+          <BottomNavigationAction  sx={{px:0,minWidth:0}} component={Link}  to={"/users/" + username}  icon={<UserAvatar width={30} height={30}  />} />
         </BottomNavigation>
   }
   </>
